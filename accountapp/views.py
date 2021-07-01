6be253b06112cddf8d8e54b7d203aa829874723f
pjw732 <86655858+pjw732@users.noreply.github.com>
2021-07-01 12:40:43 +0900

    07.01(my)

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse        # HttpResponse(화면에 띄울 값)
+from django.shortcuts import render     # render(변수, 불러올 파일)
 
 # Create your views here.
 
 
 def Hello_world(request):
-    return HttpResponse('Hello World!!')
+    return render(request, 'base.html')
