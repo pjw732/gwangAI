a3a06d9f12a2964edb3b6dab1c02dde2b4b8ebe1
pjw732 <86655858+pjw732@users.noreply.github.com>
2021-07-05 11:47:10 +0900

    07.01(my)

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render     # render(변수, 불러올 파일)
 
 
 def Hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
