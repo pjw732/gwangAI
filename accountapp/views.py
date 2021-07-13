944c3413d6dbf5ab364610c6f9aa54f00f4b8f2e
pjw732 <86655858+pjw732@users.noreply.github.com>
2021-07-13 17:21:05 +0900

    07.13_1(my)

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,16 +1,33 @@
-from django.http import HttpResponse        # HttpResponse(화면에 띄울 값)
-from django.shortcuts import render     # render(변수, 불러올 파일)
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
+from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-
-    temp = request.POST.get('input_text')
-
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html',
-                      context={'text': temp})
+
+        temp = request.POST.get('input_text')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
     else:
+        hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'POST METHOD'})
+                      context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class =UserCreationForm
+    success_url =reverse_lazy('accountapp:hello_world')
+    template_name ='accountapp/create.html'
