2ceb27f553ee446fe6f906a8c4cfc0c7e4fd623d
pjw732 <86655858+pjw732@users.noreply.github.com>
2021-07-13 17:59:19 +0900

    07.13_2(my)

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -7,5 +7,5 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name="hello_world"),
 
-    path('creat/', AccountCreateView.as_view(), name = 'create')
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -28,6 +28,6 @@ def hello_world(request):
 
 class AccountCreateView(CreateView):
     model = User
-    form_class =UserCreationForm
-    success_url =reverse_lazy('accountapp:hello_world')
-    template_name ='accountapp/create.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
